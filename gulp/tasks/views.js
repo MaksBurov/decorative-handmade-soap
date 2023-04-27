a31d7e1507a847c6f60c73ab63c022875f88c16a
MaksBurov <maksim-popov-1996@mail.ru>
2023-04-27 18:26:19 +0300

    feat: outputting a list of jobs from a file

--- gulp/tasks/views.js
+++ gulp/tasks/views.js
@@ -1,23 +1,34 @@
+const fs = require("fs");
+
 module.exports = function () {
-    $.gulp.task("views", () => {
+  $.gulp.task("views", () => {
+    const onError = function (err) {
+      $.glp.notify.onError({
+        title: "Error in views",
+        message: `File error in: ${err.message}`,
+      })(err);
 
-        const onError = function(err) {
-            $.glp.notify.onError({
-                title: "Error in views",
-                message: `File error in: ${err.message}`,
-              })(err);
-    
-            this.emit('end');
-        };
+      this.emit("end");
+    };
 
-        return $.gulp.src($.paths.views.src)
-            .pipe($.glp.plumber({
-                errorHandler: onError
-            })) 
-            .pipe($.glp.pug({
-                basedir: 'src/views'
-            }))
-            .pipe($.gulp.dest($.paths.views.dist))
-            .pipe($.browserSync.stream())
-    })
-}
+    return $.gulp
+      .src($.paths.views.src)
+      .pipe(
+        $.glp.plumber({
+          errorHandler: onError,
+        })
+      )
+      .pipe(
+        $.glp.data(function (file) {
+          return JSON.parse(fs.readFileSync("./data/data.json"));
+        })
+      )
+      .pipe(
+        $.glp.pug({
+          basedir: "src/views",
+        })
+      )
+      .pipe($.gulp.dest($.paths.views.dist))
+      .pipe($.browserSync.stream());
+  });
+};
